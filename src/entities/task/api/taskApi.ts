f28136a9b214ff04faa6ead12d02a2f90f57eec0
Leonid <leo@MacBook-Air-Leonid.local>
2026-01-29 20:07:27 +0500

    Update fetch request

--- src/entities/task/api/taskApi.ts
+++ src/entities/task/api/taskApi.ts
@@ -1,69 +1,46 @@
 const API_BASED = "http://localhost:3000/tasks";
 const headers = { "Content-Type": "application/json" };
 
-export const fetchTasks = async () => {
-  const response = await fetch(API_BASED, {
-    method: "GET",
-    headers: headers,
+const request = async (endpoint: string, options?: RequestInit) => {
+  const url = `${API_BASED}${endpoint}`;
+
+  const response = await fetch(url, {
+    ...options,
+    headers,
   });
+
   if (!response.ok) {
-    throw new Error("Ошибка загрузки");
+    throw new Error(`Ошибка запроса ${response.status}` || "Ошибка сервера");
   }
+
   return response.json();
 };
 
-export const fetchTask = async (id: string) => {
-  const response = await fetch(`${API_BASED}/${id}`, {
-    method: "GET",
-    headers: headers,
-  });
-  if (!response.ok) {
-    throw new Error("Ошибка получения задачи");
-  }
-  return response.json();
-};
+export const fetchTasks = () => request("/");
 
-export const deleteTask = async (id: string) => {
-  const response = await fetch(`${API_BASED}/${id}`, {
+export const fetchTask = (id: string) => request(`/${id}`);
+
+export const deleteTask = (id: string) =>
+  request(`/${id}`, {
     method: "DELETE",
-    headers: headers,
   });
-  if (!response.ok) {
-    throw new Error("Ошибка удаления задачи");
-  }
-  return response;
-};
 
-export const updateTask = async (
+export const updateTask = (
   id: string,
   body: { title: string; description: string; index: number },
-) => {
-  const response = await fetch(`${API_BASED}/${id}`, {
+) =>
+  request(`/${id}`, {
     method: "PUT",
-    headers: headers,
     body: JSON.stringify(body),
   });
-  console.log(response);
-  if (!response.ok) {
-    throw new Error("Ошибка обновления задачи");
-  }
-  return response;
-};
 
-export const addTask = async (body: {
+export const addTask = (body: {
   id: string;
   title: string;
   description: string;
   index: number;
-}) => {
-  const response = await fetch(`${API_BASED}`, {
+}) =>
+  request(`/`, {
     method: "POST",
-    headers: headers,
     body: JSON.stringify(body),
   });
-  console.log(response);
-  if (!response.ok) {
-    throw new Error("Ошибка обновления задачи");
-  }
-  return response;
-};
